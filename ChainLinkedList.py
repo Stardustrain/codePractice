66118e1dfa035cca41164424aaa02fc550291711
StartDust <kthanterran@gmail.com>
2016-09-13 08:56:07 +0900

    v1.01
    reference : http://interactivepython.org/runestone/static/pythonds/index.html
    
    ChainLinkedList class modifying
    : add(), getList(), size(), search();

--- ChainLinkedList.py
+++ ChainLinkedList.py
@@ -1,4 +1,5 @@
 class Node:
+
     __data=None;
     __next_p=None;
 
@@ -23,24 +24,57 @@ class Node:
 
 
 class ChainLinkedList:
-    __head=None;
-    __tail=None;
 
-    def __init__(self):
-        pass;
+    __head=None;
+    __f_node=None;
 
     def add(self, data):
         node=Node();
         node.data=data;
         node.next_p=self.__head;
+
+        if node.next_p is None:
+            self.__f_node=node;
+
         self.__head=node;
+        self.__f_node.next_p=self.__head;
 
     def getList(self):
         cur=self.__head;
+        tmpCur=self.__head;
 
-        while cur != None:
+        while cur.next_p != tmpCur:
             print(cur.data);
             cur=cur.next_p;
+        print(self.__f_node.data);
+
+    def size(self):
+        cur=self.__head;
+        tmpCur=cur;
+        l_size=0;
+
+        while cur.next_p != tmpCur:
+            l_size+=1;
+            cur=cur.next_p;
+
+        l_size+=1;
+
+        return l_size;
+
+    def search(self, data):
+        ind=0;
+        cur=self.__head;
+        l_size=(self.size())-1;
+
+        while ind<=l_size:
+            if cur.data==data:
+                return tuple([ind, cur.data]);
+            else:
+                cur=cur.next_p;
+                ind+=1;
+
+        if ind>l_size:
+            return "not found data : %d" %data;
 
 
 def main():
