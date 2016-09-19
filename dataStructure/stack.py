663eaba5d6e11942661a17a34478bb7ec92e032f
kthanterran@gmail.com <kthanterran@gmail.com>
2016-09-19 10:06:13 +0900

    20160919 first upload stack.py

---
+++ dataStructure/stack.py
@@ -0,0 +1,17 @@
+from dataStructure import LinkedList;
+
+
+class Node:
+    __data=None;
+    __next_p=None;
+
+    def __init__(self):
+        pass;
+
+
+class stack:
+    def __init__(self):
+        pass;
+
+    def push(self, data):
+        node=Node();
