9e265a6ac07e90e7184656b5b4d21f973c5b5119
kthanterran@gmail.com <kthanterran@gmail.com>
2016-11-18 23:13:42 +0900

    add dummy_LinkedList
    dummy node를 활용하여 head가 고정된 LinkedList
    head가 고정되어 if로 현재 node가 head임을 검증할 필요가 없음

---
+++ dataStructure/dummy_LinkedList.py
@@ -0,0 +1,79 @@
+class Node:
+    data = None
+    next_p = None
+
+    def __init__(self):
+        pass
+
+
+class LinkedList:
+    head = None
+    tail = None
+    _numOfSize = 0
+
+    def __init__(self):
+        self.dummy = Node()
+        self.head = self.dummy
+        self.tail = self.dummy
+
+    def add(self, data):
+        self._numOfSize += 1
+        node = Node()
+        node.data = data
+        self.tail.next_p = node
+        self.tail = node
+
+
+    def getList(self):
+        cur = self.head.next_p
+
+        while cur is not None:
+            print(cur.data)
+            cur = cur.next_p
+
+    def search(self, val):
+        cur = self.head.next_p
+        ind = 0
+
+        while cur is not None:
+            if cur.data != val:
+                cur = cur.next_p
+                ind+=1
+            else:
+                return (ind, cur.data)
+        return False
+
+    def remove(self, val):
+        cur = self.head.next_p
+        before = self.head
+
+        while cur is not None:
+            if cur.data != val:
+                before = cur
+                cur = cur.next_p
+            else:
+                before.next_p = cur.next_p
+                ret = cur.data
+                del cur
+                self._numOfSize -= 1
+                return ret
+        return False
+
+
+    def length(self):
+        return self._numOfSize
+
+
+def main():
+    ll = LinkedList()
+    ll.add(2)
+    ll.add(3)
+    ll.add(4)
+    ll.add(5)
+    ll.remove(5)
+    ll.getList()
+    ll.length()
+
+
+if __name__ == "__main__":
+    main()
